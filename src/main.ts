c33be9d50a86116e84ceab188eaaa2d7d533da5d
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-04-21 09:53:16 +0200

    Posting

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,15 @@ const LOGIN_SCHEMA = z
       val: z.string(),
     })
   );
+const POST_SCHEMA = z.object({
+  p: z.string(),
+  u: z.string(),
+});
+type Post = z.infer<typeof POST_SCHEMA>;
+const CLOUDLINK_POST_SCHEMA = z.object({
+  cmd: z.literal("direct"),
+  val: POST_SCHEMA,
+});
 
 let token: string;
 
@@ -83,4 +92,36 @@ $("#login-form").on("submit", function (e) {
 
 const loggedIn = () => {
   Modal.getInstance("#login-modal")?.hide();
+  $("#pre-logged-in").hide();
+  $("#logged-in").show();
+  const posts = $("#posts");
+  cloudlink.on("packet", (packet: unknown) => {
+    const parsed = CLOUDLINK_POST_SCHEMA.safeParse(packet);
+    if (!parsed.success) return;
+    posts.prepend(makePost(parsed.data.val));
+  });
+  $("#enter-post").on("submit", async function (e) {
+    e.preventDefault();
+    const content = $(this).find("[name=content]");
+    content.prop("disabled", true);
+    await fetch("https://api.meower.org/home", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Token: token,
+      },
+      body: JSON.stringify({
+        content: content.val(),
+      }),
+    });
+    content.prop("disabled", false);
+    content.val("");
+  });
+};
+
+const makePost = (post: Post) => {
+  const template = $("#post-template > div").clone();
+  template.find(".username").text(post.u);
+  template.find(".content").text(post.p);
+  return template;
 };
